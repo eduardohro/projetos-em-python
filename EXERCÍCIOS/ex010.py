905c2453501b2e6ff058cf192db22a7d6a077a8b
Eduardo Rodrigues Oliveira <159492869+eduardohro@users.noreply.github.com>
2024-11-01 14:29:27 -0300

    add ex010

---
+++ EXERCÍCIOS/ex010.py
@@ -0,0 +1,6 @@
+### CONVERSOR DE MOEDAS ###
+saldo = float(input('Qual o seu saldo em R$? '))
+
+dolar = saldo / 5.83 # Preço do dolar em 2024
+
+print(f'Você pode comprar US$ {dolar:.2f} com R$ {saldo:.2f}')
