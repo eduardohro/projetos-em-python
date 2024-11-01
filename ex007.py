5996d9c2188ec3360ad1d0c62d087c8720b7c4d8
Eduardo Rodrigues Oliveira <159492869+eduardohro@users.noreply.github.com>
2024-10-31 21:36:46 -0300

    add ex007

---
+++ ex007.py
@@ -0,0 +1,5 @@
+### MÉDIA ARITMÉTICA ###
+nota1 = float(input('Insira a nota do aluno no primeiro bimestre: '))
+nota2 = float(input('Insira a nota do aluno no segundo bimestre: '))
+media = (nota1 + nota2) / 2
+print(f'A média entre {nota1:.1f} e {nota2:.1f} é igual a {media:.1f}')
