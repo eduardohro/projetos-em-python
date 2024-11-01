17c6da9bf99b87ff540a4541f9b8f52fb4168aa2
Eduardo Rodrigues Oliveira <159492869+eduardohro@users.noreply.github.com>
2024-10-31 21:32:11 -0300

    add ex006

---
+++ EXERCÍCIOS/ex006.py
@@ -0,0 +1,6 @@
+### DOBRO, TRIPLO E RAIZ QUADRADA ###
+num = int(input('Digite um número: '))
+print(f'O dobro de {num} é {num*2}.')
+print(f'O triplo de {num} é {num*3}.')
+print(f'A raiz quadrada de {num} é {num ** (1/2):.2f}.')
+# pow(num, (1/2)) # Outra forma de calcular raiz quadrada
