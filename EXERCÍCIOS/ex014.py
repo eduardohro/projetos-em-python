6453d0f0591f440e91bc2239ff29337c34bc5246
Eduardo Rodrigues Oliveira <159492869+eduardohro@users.noreply.github.com>
2024-11-01 14:59:43 -0300

    add ex014

---
+++ EXERCÍCIOS/ex014.py
@@ -0,0 +1,7 @@
+### CONVERSOR DE TEMPERATURAS ###
+
+c = float(input('Informe a temperatura em ⁰C: '))
+
+f = c * 9 / 5 + 32 # Calculo para transformação de ⁰C para ⁰F
+
+print(f'A temperatura de {c} ⁰C corresponde a {f} ⁰F.')
