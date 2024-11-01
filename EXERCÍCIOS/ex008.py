024a9b6c6fb5ce0e569c93731be694cebb15d659
Eduardo Rodrigues Oliveira <159492869+eduardohro@users.noreply.github.com>
2024-10-31 21:53:01 -0300

    add ex008

---
+++ EXERCÍCIOS/ex008.py
@@ -0,0 +1,10 @@
+### CONVERSOR DE MEDIDAS ###
+d = float(input('Digite uma distância em metros: '))
+km = d / 1000
+hm = d / 100
+dam = d / 10
+dm = d * 10
+cm = d * 100
+mm = d * 1000
+print(f'A medida de {d} m corresponde a:')
+print(f'\n{km} km \n{hm} hm \n{dam} dam \n{dm} dm \n{cm} cm \n{mm} mm')
