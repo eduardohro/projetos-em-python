f8d4fe04590ddc7c67cd6874742bac498e64458b
Eduardo Rodrigues Oliveira <159492869+eduardohro@users.noreply.github.com>
2024-11-13 20:43:52 -0300

    add ex097

---
+++ EXERCÍCIOS/ex097.py
@@ -0,0 +1,11 @@
+### UM PRINT ESPECIAL ###
+
+def texto(txt):
+    print('⪖' * len(txt))
+    print(txt)
+    print('⩽' * len(txt))
+
+
+texto(f'{"  EDUARDO RODRIGUES  "}')
+texto(f'{"  CURSO DE PYTHON NO YOUTUBE  "}')
+texto(f'{"  CEV  "}')
