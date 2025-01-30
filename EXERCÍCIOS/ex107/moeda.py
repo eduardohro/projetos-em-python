ed9a2c077cc22b9b369f7aaa892fe2f637f355c4
Eduardo Rodrigues Oliveira <159492869+eduardohro@users.noreply.github.com>
2025-01-29 23:07:18 -0300

    add ex107

---
+++ EXERCÍCIOS/ex107/moeda.py
@@ -0,0 +1,37 @@
+def metade(n):
+    return n / 2
+
+
+def dobro(n):
+    return n * 2
+
+
+def aumentar(n, pc):
+    pc = n * pc/100
+    return pc + n
+
+
+def diminuir(n, pc):
+    pc = n * pc/100
+    return n - pc
+
+### CURSO EM VIDEO ###
+
+# def metade(preco):
+#     res = preco / 2
+#     return res
+
+    
+# def dobro(preco):
+#     res = preco * 2
+#     return res
+
+
+# def aumentar(preco, taxa):
+#     res = preco + (preco * taxa/100)
+#     return res
+
+
+# def diminuir(preco, taxa):
+#     res = preco - (preco * taxa/100)
+#     return res
---
+++ EXERCÍCIOS/ex107/teste.py
@@ -0,0 +1,9 @@
+### EXERCITANDO MÓDULOS EM PYTHON ####
+
+import moeda
+
+p = float((input('Digite o preço: R$ ')))
+print(f'A metade de R$ {p:.2f} é R$ {moeda.metade(p):.2f}')
+print(f'A metade de R$ {p:.2f} é R$ {moeda.dobro(p):.2f}')
+print(f'Aumentando 10%, temos R$ {moeda.aumentar(p, 10):.2f}')
+print(f'Reduzindo 13%, temos R$ {moeda.diminuir(p, 13):.2f}')
