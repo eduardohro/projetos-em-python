4a35aec1c45114ea13c8c31f354fc400846949c5
Eduardo Rodrigues Oliveira <159492869+eduardohro@users.noreply.github.com>
2025-01-30 23:15:18 -0300

    add menu

---
+++ EXERCÍCIOS/ex115/lib/interface/__init__.py
@@ -0,0 +1,17 @@
+def linha(tam=42):
+    return '-' * tam
+
+
+def cabeçalho(txt):
+    print(linha())
+    print(txt.center(42))
+    print(linha())
+
+
+def menu(lista):
+    cabeçalho('MENU PRINCIPAL')
+    c = 1
+    for item in lista:
+        print(f'\033[33m{c}\033[m - \033[34m{item}\033[m')
+        c += 1
+    print(linha())
--- EXERCÍCIOS/ex115/sistema.py
+++ EXERCÍCIOS/ex115/sistema.py
@@ -1,2 +1,18 @@
 from lib.interface import *
 from lib.arquivo import *
+from time import sleep
+
+while True:
+    resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova Pessoa', 'Sair do Sistema'])
+    if resposta == 1:
+        # Opção de listar o conteúdo de um arquivo.
+        
+    elif resposta == 2:
+        # Opção de cadastrar uma nova pessoa.
+       
+    elif resposta == 3:
+        
+        break
+    else:
+        print('\033[31mERRO! Digite uma opção válida!\033[m')
+    sleep(1)
