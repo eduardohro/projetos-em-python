0796063e5eb43fdc21e62afaac66e24f012b9447
Eduardo Rodrigues Oliveira <159492869+eduardohro@users.noreply.github.com>
2025-01-30 23:05:37 -0300

    add ex114

---
+++ EXERCÍCIOS/ex114.py
@@ -0,0 +1,13 @@
+### SITE ESTÁ ACESSÍVEL? ###
+
+import urllib
+import urllib.error
+import urllib.request
+
+try:
+    site = urllib.request.urlopen('https://eduardo-rodrigues.vercel.app/')
+except urllib.error.URLError:
+    print('\033[31mO site Eduardo Rodrigues não está acessível no momento.\033[m')
+else:
+    print('\033[32mConsegui acessar o site Eduardo Rodrigues com sucesso!\033[m')
+    # print(site.read())
